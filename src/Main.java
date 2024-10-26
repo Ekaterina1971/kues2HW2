ed3d3b6804ef0a1b47e917e8b2beb20196e015d7
Ekaterina1971 <ivanova71dom@yandex.ru>
2024-10-26 23:44:04 +0300

    готовая работа

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
-public class Main {
-    public static void main(String[] args) {
 
+public class Main {
+
+    public static void main(String[] args) {
+        ValidationService.validateLogin("Africa");
+        ValidationService.validatePassword("765tt", "765tt");
+        try {
+            ValidationService.validateLogin();
+        } catch (WrongLoginException exception) {
+            System.out.println("Логин:%s");
+        }
+        try {
+            ValidationService.validatePassword();
+        } catch (WrongPasswordException exception) {
+            System.out.println("Пароль:%s");
+        }
+        System.out.println("Все параметры верны");
     }
+
 }
